0b5be7e876b6f608a77cb479c3946326e05cf41f
hyunjunbaek <hjbaek5796@gmail.com>
2025-05-22 17:38:30 +0900

    Test variables

--- Obstacle-Dodge/Assets/Scripts/Mover.cs
+++ Obstacle-Dodge/Assets/Scripts/Mover.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    float xValue = 0f;
+    float yValue = 0.05f;
+    float zValue = 0f;
+
+
     void Start()
     {
         
@@ -10,6 +15,6 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        transform.Translate(xValue, yValue, zValue);
     }
 }

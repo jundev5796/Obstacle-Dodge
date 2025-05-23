1818ef2e5e1992942338f6c6d7b7525a2d91f1c8
hyunjunbaek <hjbaek5796@gmail.com>
2025-05-23 18:10:45 +0900

    GetComponent function

--- Obstacle-Dodge/Assets/Scripts/ObjectHit.cs
+++ Obstacle-Dodge/Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other)
     {
+        GetComponent<MeshRenderer>().material.color = Color.green;
         Debug.Log("Something hit me!");
     }
 }

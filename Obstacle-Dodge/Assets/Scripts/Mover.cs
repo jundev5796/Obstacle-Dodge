786f68b79f604af703caa4d473d68af4ee27f5b7
hyunjunbaek <hjbaek5796@gmail.com>
2025-05-23 17:50:58 +0900

    Test Methods

--- Obstacle-Dodge/Assets/Scripts/Mover.cs
+++ Obstacle-Dodge/Assets/Scripts/Mover.cs
@@ -6,15 +6,26 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
-        
+        PrintInStruction();
     }
 
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInStruction()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move using arrow keys or wasd");
+        Debug.Log("Don't bump into Objects!");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
         transform.Translate(xValue, yValue, zValue);
     }
 }

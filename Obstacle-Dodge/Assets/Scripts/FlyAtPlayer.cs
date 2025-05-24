814603fb3992381755f2c3ccc4143f7731e3724d
hyunjunbaek <hjbaek5796@gmail.com>
2025-05-24 19:07:28 +0900

    Add projectile

---
+++ Obstacle-Dodge/Assets/Scripts/FlyAtPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FlyAtPlayer : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Transform player;
+    Vector3 playerPosition;
+
+    void Start()
+    {
+        playerPosition = player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, moveSpeed * Time.deltaTime);
+    }
+}

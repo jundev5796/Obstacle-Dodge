1bff97a2eb244629e784ccd7e1d4ecd7e250e328
hyunjunbaek <hjbaek5796@gmail.com>
2025-05-24 11:24:58 +0900

    Getting reference

--- Obstacle-Dodge/Assets/Scripts/Dropper.cs
+++ Obstacle-Dodge/Assets/Scripts/Dropper.cs
@@ -4,16 +4,24 @@ public class Dropper : MonoBehaviour
 {
     [SerializeField] float timeToWait = 3;
 
+    MeshRenderer myMeshRenderer;
+    Rigidbody myRigidbody;
+
     void Start()
     {
-        
+        myMeshRenderer = GetComponent<MeshRenderer>();
+        myMeshRenderer.enabled = false;
+
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.useGravity = false;
     }
 
     void Update()
     {
         if (Time.time > timeToWait)
         {
-            Debug.Log("Lookout Below!");
+            myMeshRenderer.enabled = true;
+            myRigidbody.useGravity = true;
         }
     }
 }
